8bdd6079d5966cc471dd11ca10c386443edc43f2
ketongwang <kentwang16@gmail.com>
2014-10-01 21:31:51 -0500

    plot_convergence finished

--- plot_convergence.m
+++ plot_convergence.m
@@ -1,12 +1,26 @@
 function f = plot_convergence(M)
-  n = dim(M)(2)
-  ncol_plot = ceil(sqrt(n));
-  nrow_plot = ceil(n / ncol_plot)
-  for i in 1:dim(M)(2)
-    
-  subplot(3, 2, 1); plot(M(:, 1)); title('\beta_0'); xlabel("Iteration");
-  subplot(3, 2, 2); plot(M(:, 2)); title('\beta_1'); xlabel("Iteration");
-  subplot(3, 2, 3); plot(M(:, 3)); title('\beta_2'); xlabel("Iteration");
-  subplot(3, 2, 4); plot(M(:, 4)); title('\beta_3'); xlabel("Iteration");
-  subplot(3, 2, 5:6); plot(M(:, 5)); title('Deviance: \lambda(\beta)'); xlabel("Iteration");
-  suptitle("NB regression convergence using OLS as starting value for IRWLS");
+  n = size(M)(2)
+  ncol_plot = 2;
+  nrow_plot = ceil(n / ncol_plot);
+  
+  %- Reserved first graph for deviance
+  figure;
+  subplot(nrow_plot, ncol_plot, 1:2);
+  plot(M(:, 1));
+  title('Deviance: \lambda(\beta)');
+  xlabel("Iteration");
+  
+  for i = 2:nrow_plot
+    for j = 1:ncol_plot
+      ind = (i - 1) * ncol_plot + j - 1;
+      if ind - 1 <= n
+        subplot(nrow_plot, ncol_plot, ind + 1);
+        plot(M(:, ind));
+        title(strcat('\beta_', int2str(ind - 2)));
+        xlabel("Iteration");
+      else
+        break;
+      endif
+    endfor
+  endfor
+  suptitle("Convergence of IRWLS");
