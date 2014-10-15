d107f1bc85b83d9f4d9dc7c6e8e304c3fb84e6b1
ketongwang <kentwang16@gmail.com>
2014-10-15 01:14:14 -0500

    hw 4 test running

--- v_2/hw4.m
+++ v_2/hw4.m
@@ -2,3 +2,9 @@ data = csvread("hw4_3.csv");
 X = [ones(size(data)(1), 1), data(:, 1)];
 n = data(:, 2);
 y = data(:, 3);
+
+
+% Test run format glmReg(X, y, family, link, canonical, n = 0)
+
+%- Poisson and logit test
+glmReg(X, y, "Poisson", "logit", false, n);
