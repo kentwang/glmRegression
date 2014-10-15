d616428a4e4e0877bb9e78947cde0ad042fb227e
ketongwang <kentwang16@gmail.com>
2014-10-15 16:42:09 -0500

    Test successful for Poisson + logit

--- v_2/hw4.m
+++ v_2/hw4.m
@@ -7,10 +7,10 @@ y = data(:, 3);
 % Test run format glmReg(X, y, family, link, canonical, n = 0)
 
 %- Logistic regression + logit
-glmReg(X, y, "Binomial", "logit", true, n);
+beta_logistic_logit = glmReg(X, y, "Binomial", "logit", true, n);
 
 %- Logistic regression + probit
-glmReg(X, y, "Binomial", "probit", false, n);
+beta_logistic_probit = glmReg(X, y, "Binomial", "probit", false, n);
 
 %- Poisson and logit test
 glmReg(X, y, "Poisson", "logit", false, n);
