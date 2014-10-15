65776d9c8e0e5463a6ae1837eac1171f68ce4892
ketongwang <kentwang16@gmail.com>
2014-10-14 21:23:05 -0500

    n

--- inv_link_binomial.m
+++ inv_link_binomial.m
@@ -1,3 +1,3 @@
 %- This is the inverse link function of binomial count. For calculation of mu, expected count out of n
 function ginv = inv_link_binomial(n, eta)
-	ginv = n * exp(eta) ./ (1 + exp(eta)); % notice the dot operation
+	ginv = n. * exp(eta) ./ (1 + exp(eta)); % notice the dot operation
