c7377071ab775e2570267759b73cfa71535267fe
ketongwang <kentwang16@gmail.com>
2014-10-20 15:35:10 -0500

    Augmented with logistic regression modification

--- v_2/logl_Binomial.m
+++ v_2/logl_Binomial.m
@@ -1,3 +1,11 @@
 %- Choose is not included Binomial coefficients [log(NchooseK(n, y))]
+%- Note, y is modified for logistic regression. Plus a noise
 function ll = logl_Binomial(mu, n, y)
+  if unique(n) == 1
+    for i = 1:length(y)
+      if y(i) == 0
+        y(i) = 0.00001;
+      endif
+    endfor
+  endif
   ll = sum(y.*log(mu./y) + (n-y).*log((n-mu)./n));
