e7e8d3540d9fdaa09f0c3f5f019843aeab7c790e
ketongwang <kentwang16@gmail.com>
2014-10-20 22:34:00 -0500

    1, Updating z should be very careful 2, Save and plot convergence

--- v_2/poisZeroInflatedEM.m
+++ v_2/poisZeroInflatedEM.m
@@ -16,6 +16,22 @@
 function [beta_new, gamm_new, z] = poisInfEM(X, y, z0, epsilon = 10^-6)
   beta_old = gamma_old = repmat(100, size(X)(2), 1);
   z = z0;
-  beta_new = 
-  gamma_new = 
-  while()
+  beta_new = glmReg(X, y, "Poisson", "log", true, repmat(1, length(z0), 1), 1 - z, false);
+  gamma_new = glmReg(X, z, "Binomial", "logit", true, repmat(1, length(z0), 1), 0, false);
+  
+  iter = 0;
+  while(sum(abs(beta_new - beta_old)) / sum(abs(beta_old)) > epsilon || sum(abs(gamma_new - gamma_old)) / sum(abs(gamma_old)) > epsilon)
+    iter += 1;
+    printf('Iteration %d\n', iter);
+    printf('Beta_new, ');
+    disp(beta_new');
+    printf('Gamma_new, ');
+    disp(gamma_new');
+    
+    % Updating z should be careful
+    z = 1./(1 + exp(-X*gamma_new - exp(X*beta_new)));
+    beta_old = beta_new;
+    gamma_old = gamma_new;
+    beta_new = glmReg(X, y, "Poisson", "log", true, repmat(1, length(z0), 1), 1 - z, false);
+    gamma_new = glmReg(X, z, "Binomial", "logit", true, repmat(1, length(z0), 1), 0, false);
+  endwhile
