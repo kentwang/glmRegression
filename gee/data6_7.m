48f73573c0f290c6925b542cc77b9ed4aa013732
Ketong Wang <kentwang16@gmail.com>
2014-11-18 19:24:44 -0600

    No comment

--- gee/data6_7.m
+++ gee/data6_7.m
@@ -1,4 +1,4 @@
-data = csvread("epilepsy");
+tre_y_raw = dlmread("epilepsy_tre_y.txt", sep = "\t");
 base_age_raw = dlmread ("epilepsy_base_age.txt", sep = " ");
 base = real(base_age_raw);
 age = imag(base_age_raw);
